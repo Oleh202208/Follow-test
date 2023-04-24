550ebf044bc5bc501674b212a20620fcad32d587
Oleh202208 <110285061+Oleh202208@users.noreply.github.com>
2023-04-24 17:01:36 +0000

    Deploying to gh-pages from @ Oleh202208/Follow-test@472e859d6f4aacc4c940b10ce7f2ba3382e1405a 🚀

---
+++ static/js/335.a875e6b4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfollow_test=self.webpackChunkfollow_test||[]).push([[335],{335:function(e,t,l){l.r(t);var n=l(184);t.default=function(){return(0,n.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=335.a875e6b4.chunk.js.map
